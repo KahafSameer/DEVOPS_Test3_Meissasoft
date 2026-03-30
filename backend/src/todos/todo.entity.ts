0f6276504fc3e3e3508a82917404a04d20e712ff
kahafsameer <sameerkahaf920@gmail.com>
2026-03-30 17:11:22 +0500

    Initial commit: backend Nest + frontend Next

---
+++ backend/src/todos/todo.entity.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
+
+@Entity({ name: 'todos' })
+export class Todo {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 255 })
+  title: string;
+
+  @Column({ type: 'text', nullable: true })
+  description: string | null;
+
+  @Column({ default: false })
+  isCompleted: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
+
